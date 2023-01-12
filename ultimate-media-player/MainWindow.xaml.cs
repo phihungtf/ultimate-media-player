c7af3d90753a89fd353322df736a940ac87ae723
20120474 <84512387+kimhieu153255@users.noreply.github.com>
2023-01-12 15:45:45 +0700

    main gui

--- ultimate-media-player/MainWindow.xaml.cs
+++ ultimate-media-player/MainWindow.xaml.cs
@@ -12,14 +12,59 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
-namespace ultimate_media_player {
+namespace WpfApp1
+{
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
             InitializeComponent();
+        }
+        private void ShowVideo_Click(object sender, RoutedEventArgs e)
+        {
+            if (lvPlayList.Visibility == Visibility.Visible)
+            {
+                lvPlayList.Visibility = Visibility.Collapsed;
+                media.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                lvPlayList.Visibility = Visibility.Visible;
+                media.Visibility = Visibility.Collapsed;
+            }
+        }
+        string _currentPlaying = "";
+        bool _isFullScreen = false;
+        private void Show(object sender, RoutedEventArgs e)
+        {
+            var screen = new OpenFileDialog();
+            if (screen.ShowDialog() == true)
+            {
+                _currentPlaying = screen.FileName;
+                media.Source = new Uri(_currentPlaying, UriKind.Absolute);
+                media.Play();
+            }
+        }
+
+        private void FullScreen(object sender, RoutedEventArgs e)
+        {
+            if (_isFullScreen)
+            {
+                _isFullScreen = false;
+            }
+            else
+            {
+                _isFullScreen = true;
+                Main.Children.Remove(media);
+                this.Content = media;
+                this.WindowStyle = WindowStyle.None;
+                this.WindowState = WindowState.Maximized;
+            }
         }
     }
 }
