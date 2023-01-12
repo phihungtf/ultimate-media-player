56e5b340e6bf944008e834dbb76015e34e68cd58
20120474 <84512387+kimhieu153255@users.noreply.github.com>
2023-01-13 05:14:06 +0700

    Add all files to Playlist

--- ultimate-media-player/MainWindow.xaml.cs
+++ ultimate-media-player/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using Microsoft.Win32;
 
 namespace WpfApp1
@@ -40,6 +43,7 @@ namespace WpfApp1
         }
         string _currentPlaying = "";
         bool _isFullScreen = false;
+
         private void Show(object sender, RoutedEventArgs e)
         {
             var screen = new OpenFileDialog();
@@ -47,7 +51,10 @@ namespace WpfApp1
             {
                 _currentPlaying = screen.FileName;
                 media.Source = new Uri(_currentPlaying, UriKind.Absolute);
+                lvPlayList.Visibility = Visibility.Collapsed;
+                media.Visibility = Visibility.Visible;
                 media.Play();
+                media.Stop();
             }
         }
 
@@ -73,7 +80,55 @@ namespace WpfApp1
             lvPlayList.Width = Main.ActualWidth;
             slider_video.Width = Main.ActualWidth - 200;
             double temp = (Main.ActualWidth - 440) / 2;
-            mid_controller.Margin = new Thickness( temp, 0, temp,0);
+            mid_controller.Margin = new Thickness(temp+60, 0, temp-100, 0);
+            title_column.Width = Main.ActualWidth-200;
+        }
+
+        public class Video{
+            public String path { get; set; } = "";
+            public String title  {
+                get {
+                    var infor = new FileInfo(path);
+                    return infor.Name;
+                }
+            }
+            public String duration { get; set; } = "";
+
+        }
+
+        public BindingList<Video> videoList { get; set; } = new BindingList<Video>();
+
+        private void AddFileToPlaylist(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.Filter = 
+            openFileDialog.Multiselect = true;
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string filename in openFileDialog.FileNames)
+                {
+                    Video video = new Video();
+                    video.path = filename;
+                    MediaPlayer mediaDuration = new MediaPlayer();
+                    mediaDuration.Open(new Uri(filename));
+                    while (!mediaDuration.NaturalDuration.HasTimeSpan) ;
+                    int hours = mediaDuration.NaturalDuration.TimeSpan.Hours;
+                    int minutes = mediaDuration.NaturalDuration.TimeSpan.Minutes;
+                    int seconds = mediaDuration.NaturalDuration.TimeSpan.Seconds;
+                    video.duration = $"{hours}:{minutes}:{seconds}";
+                    videoList.Add(video);
+                }
+                lvPlayList.ItemsSource = videoList;
+            }
+        }
+
+        private void media_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            int hours = media.NaturalDuration.TimeSpan.Hours;
+            int minutes = media.NaturalDuration.TimeSpan.Minutes;
+            int seconds = media.NaturalDuration.TimeSpan.Seconds;
+            duration.Text = $"{hours}:{minutes}:{seconds}";
         }
     }
 }
